f47de3afdf18a76581994dd3fc0e90087c80b34d
Dylan Lacey <github@dylanlacey.com>
2013-01-21 13:34:13 -0800

    Stubbed out config for sauce_spec, so now passes

--- spec/sauce_spec.rb
+++ spec/sauce_spec.rb
@@ -5,6 +5,11 @@ describe Heroku::Command::Sauce do
 
   describe '#scoutup!' do
     context 'when configured' do
+      before(:each) do
+        @config = command.instance_variable_get(:@config)
+        @config.stub(:configured? => true)
+      end
+
       it 'should create a scout session' do
         api = command.instance_variable_get(:@sauceapi)
         api.should_receive(:create_scout_session)
